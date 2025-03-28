39fa4fe8af43885ae73c7cab0f1444b181745905
xAarT <arturbresolin20@gmail.com>
2025-03-27 21:20:01 -0300

    Adicionado possibilidade de personalização do fundo

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("meuCanvas")
 const ctx = canvas.getContext("2d")
 
+const rangeQtd = document.querySelector(".qtdPontos")
+const rangeVlc = document.querySelector(".vlcPontos")
+
 function ajustarCanvas() {
     const dpr = window.devicePixelRatio || 1
     canvas.width = window.innerWidth * dpr
@@ -10,26 +13,45 @@ function ajustarCanvas() {
 
 ajustarCanvas()
 
+let qtdP = parseInt(rangeQtd.value) || 0
+let vlcP = parseFloat(rangeVlc.value) || 0
+
 let arrPontos = []
 
-for (let i = 0; i < 1000; i++) {
-    const tamanho = Math.random() * 3 + 1
-    arrPontos.push({
-        s: tamanho,
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        vx: 0.01 * tamanho * (Math.random() < 0.5 ? -1 : 1),
-        vy: 0.01 * tamanho * (Math.random() < 0.5 ? -1 : 1)
-    })
+function criarPontos() {
+    arrPontos = [];
+    for (let i = 0; i < qtdP; i++) {
+        const tamanho = Math.random() * 3 + 1
+        arrPontos.push({
+            s: tamanho,
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            vx: vlcP * tamanho * (Math.random() < 0.5 ? -1 : 1),
+            vy: vlcP * tamanho * (Math.random() < 0.5 ? -1 : 1)
+        })
+    }
 }
 
+rangeQtd.addEventListener("change", function() {
+    qtdP = parseInt(rangeQtd.value) || 0
+    criarPontos()
+})
+
+rangeVlc.addEventListener("change", function() {
+    vlcP = parseFloat(rangeVlc.value) || 0
+    criarPontos()
+})
+
+criarPontos()
+
 function atualizarPontos() {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
+    ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     arrPontos.forEach(ponto => {
         ponto.x += ponto.vx
         ponto.y += ponto.vy
+
         if (ponto.x <= 0 || ponto.x >= canvas.width) {
             ponto.vx *= -1
         }
